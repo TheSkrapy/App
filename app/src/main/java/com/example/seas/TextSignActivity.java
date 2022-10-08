67ef66d7dbc807494d281e36c80cfefb55f9d42f
Eduardo-Hernande <L19580609@reynosa.tecnm.mx>
2022-10-07 22:34:19 -0500

    TextSignActivity creada

---
+++ app/src/main/java/com/example/seas/TextSignActivity.java
@@ -0,0 +1,15 @@
+package com.example.seas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TextSignActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_text_sign);
+
+    }
+}
