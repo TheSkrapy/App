da9863082308256187d4a624f0bd3273337eb26e
Eduardo-Hernande <L19580609@reynosa.tecnm.mx>
2022-10-09 17:47:06 -0500

    Text_sign E2 traduccion_text_sign función add sign afTextChanged finalizada

--- app/src/main/java/com/example/seas/TextSignActivity.java
+++ app/src/main/java/com/example/seas/TextSignActivity.java
@@ -1,6 +1,5 @@
 package com.example.seas;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 
@@ -53,12 +52,12 @@ public class TextSignActivity extends AppCompatActivity {
                 //Detect if user add or delete a char
                 if (str.length() > nChar[0]){
                     //add sign
-                    signTranslate.add(signList.get(getMeanId(signList, getChar(str, nChar))));
+                    int id = getMeanId(signList, getChar(str, nChar));
+                    signTranslate.add(signList.get(id));
                     binding.tsRecyclerView.setAdapter(new CardAdapter(signTranslate));
                 }
                 else{
                     //remove sign
-
                     nChar[0] -= 1;
                 }
 
@@ -95,7 +94,8 @@ public class TextSignActivity extends AppCompatActivity {
     private int getMeanId (List<Sign> listOrigin, char c) {
         int j=0;
         for (int i = 0; i<listOrigin.size(); i++){
-            if (listOrigin.get(i).mean == c){
+            char c1 = listOrigin.get(i).mean;
+            if (c1 == c || chToLowerCase(c1) == c){
                 j=i;
                 break;
             }
@@ -105,16 +105,40 @@ public class TextSignActivity extends AppCompatActivity {
     }
 
     private void createSigns (){
-        Sign sign1 = new Sign(R.drawable.ic_baseline_keyboard_voice_24, signList.size(), 'A');
-        signList.add(sign1);
-        Sign sign2 = new Sign(R.drawable.ic_baseline_keyboard_voice_24, signList.size(), 'B');
-        signList.add(sign2);
-        Sign sign3 = new Sign(R.drawable.ic_baseline_keyboard_voice_24, signList.size(), 'C');
-        signList.add(sign3);
-        Sign sign4 = new Sign(R.drawable.ic_baseline_keyboard_voice_24, signList.size(), 'D');
-        signList.add(sign4);
-        signList.add(sign1);
-        signList.add(sign2);
+        signList.add(new Sign(R.drawable.img_a, signList.size(), 'A'));
+        signList.add(new Sign(R.drawable.img_b, signList.size(), 'B'));
+        signList.add(new Sign(R.drawable.img_c, signList.size(), 'C'));
+        signList.add(new Sign(R.drawable.img_d, signList.size(), 'D'));
+
+        signList.add(new Sign(R.drawable.img_e, signList.size(), 'E'));
+        signList.add(new Sign(R.drawable.img_f, signList.size(), 'F'));
+        signList.add(new Sign(R.drawable.img_g, signList.size(), 'G'));
+        signList.add(new Sign(R.drawable.img_h, signList.size(), 'H'));
+        signList.add(new Sign(R.drawable.img_d, signList.size(), 'I'));
+        signList.add(new Sign(R.drawable.img_j, signList.size(), 'J'));
+        signList.add(new Sign(R.drawable.img_k, signList.size(), 'K'));
+        signList.add(new Sign(R.drawable.img_l, signList.size(), 'L'));
+        signList.add(new Sign(R.drawable.img_m, signList.size(), 'M'));
+        signList.add(new Sign(R.drawable.img_n, signList.size(), 'N'));
+        signList.add(new Sign(R.drawable.img_o, signList.size(), 'O'));
+        signList.add(new Sign(R.drawable.img_p, signList.size(), 'P'));
+        signList.add(new Sign(R.drawable.img_q, signList.size(), 'Q'));
+        signList.add(new Sign(R.drawable.img_r, signList.size(), 'R'));
+        signList.add(new Sign(R.drawable.img_s, signList.size(), 'S'));
+        signList.add(new Sign(R.drawable.img_t, signList.size(), 'T'));
+        signList.add(new Sign(R.drawable.img_u, signList.size(), 'U'));
+        signList.add(new Sign(R.drawable.img_v, signList.size(), 'V'));
+        signList.add(new Sign(R.drawable.img_w, signList.size(), 'W'));
+        signList.add(new Sign(R.drawable.img_x, signList.size(), 'X'));
+        signList.add(new Sign(R.drawable.img_y, signList.size(), 'Y'));
+        signList.add(new Sign(R.drawable.img_z, signList.size(), 'Z'));
+    }
+
+    private char chToLowerCase(char c1){
+        int temp = (int)c1;
+        temp = temp+32;
+        c1=(char)temp;
+        return c1;
     }
 
 
